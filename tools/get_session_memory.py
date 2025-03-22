a54d0b5d850f40b19ce9f7aaa21d3ad833e65233
Obada Khalili <obada.akhalili@gmail.com>
2025-03-22 11:58:18 +0200

    update get_session_memory to only get memory and remove steps to init session and send user message, make add_session_memory more general to add user message as well as assistant message, add new tool to init session

--- tools/get_session_memory.py
+++ tools/get_session_memory.py
@@ -6,33 +6,13 @@ from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 from zep_cloud.client import Zep
-from zep_cloud import Message
 
 
 class GetSessionMemoryTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         try:
             api_key = self.runtime.credentials["zep_api_key"]
-
             client = Zep(api_key=api_key)
-
-            try:
-                client.memory.add_session(
-                    user_id=tool_parameters["user_id"],
-                    session_id=tool_parameters["session_id"],
-                )
-            except:
-                pass
-
-            client.memory.add(
-                session_id=tool_parameters["session_id"],
-                messages=[
-                    Message(
-                        content=tool_parameters["user_message"],
-                        role_type="user",
-                    ),
-                ],
-            )
 
             memory = client.memory.get(
                 session_id=tool_parameters["session_id"],
--- tools/init_session.py
+++ tools/init_session.py
@@ -5,24 +5,23 @@ from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
 
 from zep_cloud.client import Zep
-from zep_cloud import Message
+from zep_cloud.errors import BadRequestError
 
 
-class AddSessionMemoryTool(Tool):
+class InitSessionTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         try:
             api_key = self.runtime.credentials["zep_api_key"]
             client = Zep(api_key=api_key)
 
-            client.memory.add(
-                session_id=tool_parameters["session_id"],
-                messages=[
-                    Message(
-                        content=tool_parameters["message"],
-                        role_type=tool_parameters["role_type"],
-                    ),
-                ],
-            )
+            try:
+                client.memory.add_session(
+                    user_id=tool_parameters["user_id"],
+                    session_id=tool_parameters["session_id"],
+                )
+            except BadRequestError as e:
+                # bad request error could only happen if the session already exists, which is fine
+                pass
 
             yield self.create_json_message({"status": "success"})
         except Exception as e:
