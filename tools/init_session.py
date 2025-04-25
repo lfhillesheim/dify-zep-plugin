c2786b4955b523903a7c436164f57fda47fcbcce
Obada Khalili <obada.akhalili@gmail.com>
2025-04-25 21:32:13 +0300

    Revert "include link to credentials page"
    This reverts commit 082a356d319fff9efeb8f2cab3115098c868c658.

--- tools/init_session.py
+++ tools/init_session.py
@@ -1,34 +1,27 @@
 from collections.abc import Generator
 from typing import Any
 
-import requests
-
 from dify_plugin import Tool
 from dify_plugin.entities.tool import ToolInvokeMessage
+
+from zep_cloud.client import Zep
+from zep_cloud.errors import BadRequestError
 
 
 class InitSessionTool(Tool):
     def _invoke(self, tool_parameters: dict[str, Any]) -> Generator[ToolInvokeMessage]:
         try:
             api_key = self.runtime.credentials["zep_api_key"]
-            headers = {
-                "Authorization": f"Api-Key {api_key}",
-                "Content-Type": "application/json",
-            }
-            data = {
-                "session_id": tool_parameters["session_id"],
-                "user_id": tool_parameters["user_id"],
-            }
+            client = Zep(api_key=api_key)
 
-            response = requests.post(
-                "https://api.getzep.com/api/v2/sessions", headers=headers, json=data
-            )
-
-            if response.status_code == 400:
-                # 400 status code means the session already exists, which is fine
+            try:
+                client.memory.add_session(
+                    user_id=tool_parameters["user_id"],
+                    session_id=tool_parameters["session_id"],
+                )
+            except BadRequestError as e:
+                # bad request error could only happen if the session already exists, which is fine
                 pass
-            elif not response.ok:
-                response.raise_for_status()
 
             yield self.create_json_message({"status": "success"})
         except Exception as e:
